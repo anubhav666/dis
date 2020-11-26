1fe09e87b4b45f09b865affd751e146a97b44708
Anubhav Dutta <meliodasanubhavdutta@gmail.com>
2020-11-26 14:46:16 +0530

    New Portfolio JS

---
+++ main.js
@@ -0,0 +1,37 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray= [" Das.", ", a Web Developer.", ", a Competitive Programmer.", ", a ML/AI Enthusiast." ];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type()
+{
+    if(charIndex < textArray[textArrayIndex].length){
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    
+    charIndex++;
+    setTimeout(type, typingDelay);
+    }
+    else{
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if(charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else{
+        textArrayIndex++;
+        if(textArrayIndex>=textArray.length) textArrayIndex=0;
+        setTimeout(type, typingDelay +1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, newTextDelay + 250);
+});
